08d3d50559f6f24760df5771ffc4eddcb32156ce
Jayadeep KM <kmjayadeep@gmail.com>
2018-10-04 21:44:36 +0530

    added test cases for all types

--- test/test.js
+++ test/test.js
@@ -10,6 +10,33 @@ describe('ordinalOf_1()', function () {
   });
 });
 
+describe('ordinalOf_2()', function () {
+  it('should return number with its ordinal', function () {
+    const x = 2;
+    const xOrdinal = '2nd';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
+});
+
+describe('ordinalOf_3()', function () {
+  it('should return number with its ordinal', function () {
+    const x = 3;
+    const xOrdinal = '3rd';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
+});
+
+describe('ordinalOf_5()', function () {
+  it('should return number with its ordinal', function () {
+    const x = 5;
+    const xOrdinal = '5th';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
+});
+
 describe('ordinalOf_a()', function () {
     it('should return error', function () {
       const x = 'a';
@@ -19,13 +46,22 @@ describe('ordinalOf_a()', function () {
     });
 });
 
+describe('ordinalOf_10()', function () {
+  it('should return number with its ordinal', function () {
+    const x = 10;
+    const xOrdinal = '10th';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
+});
+
 describe('ordinalOf_11()', function () {
-    it('should return number with its ordinal', function () {
-      const x = 11;
-      const xOrdinal = '11th';
-      var y = o.toOrdinal(x);
-      expect(y).to.be.equal(xOrdinal);
-    });
+  it('should return number with its ordinal', function () {
+    const x = 11;
+    const xOrdinal = '11th';
+    var y = o.toOrdinal(x);
+    expect(y).to.be.equal(xOrdinal);
+  });
 });
 
 describe('ordinalOf_13()', function () {
