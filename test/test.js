6aaf7e1b534c76995877b75979da00d91e655038
Jayadeep KM <kmjayadeep@gmail.com>
2018-10-05 08:39:59 +0530

    converted single quotes to double

--- test/test.js
+++ test/test.js
@@ -1,82 +1,82 @@
-const expect = require('chai').expect;
-const o = require('../index.js');
+const expect = require("chai").expect;
+const o = require("../index.js");
 
-describe('ordinalOf_1()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_1()", function () {
+  it("should return number with its ordinal", function () {
     const x = 1;
-    const xOrdinal = '1st';
+    const xOrdinal = "1st";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_2()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_2()", function () {
+  it("should return number with its ordinal", function () {
     const x = 2;
-    const xOrdinal = '2nd';
+    const xOrdinal = "2nd";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_3()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_3()", function () {
+  it("should return number with its ordinal", function () {
     const x = 3;
-    const xOrdinal = '3rd';
+    const xOrdinal = "3rd";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_5()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_5()", function () {
+  it("should return number with its ordinal", function () {
     const x = 5;
-    const xOrdinal = '5th';
+    const xOrdinal = "5th";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_a()', function () {
-    it('should return error', function () {
-      const x = 'a';
-      const xOrdinal = 'a is not a valid number';
+describe("ordinalOf_a()", function () {
+    it("should return error", function () {
+      const x = "a";
+      const xOrdinal = "a is not a valid number";
       var y = o.toOrdinal(x);
       expect(y).to.be.equal(xOrdinal);
     });
 });
 
-describe('ordinalOf_10()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_10()", function () {
+  it("should return number with its ordinal", function () {
     const x = 10;
-    const xOrdinal = '10th';
+    const xOrdinal = "10th";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_11()', function () {
-  it('should return number with its ordinal', function () {
+describe("ordinalOf_11()", function () {
+  it("should return number with its ordinal", function () {
     const x = 11;
-    const xOrdinal = '11th';
+    const xOrdinal = "11th";
     var y = o.toOrdinal(x);
     expect(y).to.be.equal(xOrdinal);
   });
 });
 
-describe('ordinalOf_13()', function () {
-    it('should return number with its ordinal', function () {
+describe("ordinalOf_13()", function () {
+    it("should return number with its ordinal", function () {
       const x = 13;
-      const xOrdinal = '13th';
+      const xOrdinal = "13th";
       var y = o.toOrdinal(x);
       expect(y).to.be.equal(xOrdinal);
     });
 });
 
-describe('ordinalOf_21()', function () {
-    it('should return number with its ordinal', function () {
+describe("ordinalOf_21()", function () {
+    it("should return number with its ordinal", function () {
       const x = 21;
-      const xOrdinal = '21st';
+      const xOrdinal = "21st";
       var y = o.toOrdinal(x);
       expect(y).to.be.equal(xOrdinal);
     });
